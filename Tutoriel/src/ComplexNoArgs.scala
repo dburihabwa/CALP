3a04f5a6b87d86d9949b1f1dc3778a7113cfeeed
Dorian Burihabwa <dorian@burihabwa.com>
2014-01-13 20:34:49 +0100

    Tutoriel
    Ajout du premier TP sur le turoriel Scala

---
+++ Tutoriel/src/ComplexNoArgs.scala
@@ -0,0 +1,4 @@
+class ComplexNoArgs(real : Double, imaginary : Double) {
+	def re = real
+	def im = imaginary
+}
---
+++ Tutoriel/src/Reference.scala
@@ -0,0 +1,5 @@
+class Reference[T] {
+  private var contents: T = _
+  def set(value: T) { contents = value }
+  def get: T = contents
+}
